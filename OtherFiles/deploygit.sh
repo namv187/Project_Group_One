67196b529f734bce294a3c053009ba9d0e68db0b
vboxuser <vboxuser@proj480-del.myguest.virtualbox.org>
2024-11-15 00:42:35 -0800

    m

--- OtherFiles/deploygit.sh
+++ OtherFiles/deploygit.sh
@@ -2,27 +2,13 @@
 
 # Variables
 REPO_URL="https://github.com/Alexisb710/project480.git"
-DEST_DIR="/var/www/project480"
-
-# Remove existing content from the Apache web directory (optional, be cautious)
-echo "Removing old website files from $DEST_DIR ..."
-sudo rm -rf $DEST_DIR/*
-
-# Clone the GitHub repository into a temporary directory
-TMP_DIR=$(mktemp -d)
-echo "Cloning repository from $REPO_URL into $TMP_DIR ..."
-git clone $REPO_URL $TMP_DIR
-
-# Move the content from the cloned repository to the Apache web directory
-echo "Moving files to $DEST_DIR ..."
-sudo mv $TMP_DIR/* $DEST_DIR
+DEST_DIR="/var/www/"
+cd $DEST_DIR
+git clone $REPO_URL
 
 # Set appropriate permissions for Apache
 echo "Setting correct permissions for Apache..."
 sudo chown -R www-data:www-data $DEST_DIR
 sudo chmod -R 755 $DEST_DIR
-
-# Remove the temporary directory
-rm -rf $TMP_DIR
 
 echo "Website files have been deployed to $DEST_DIR."

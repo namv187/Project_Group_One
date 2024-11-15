1d55ab1c2511af97c849244e12aaa01c2652eccf
vboxuser <vboxuser@project480.myguest.virtualbox.org>
2024-11-14 22:55:05 -0800

    fix more scripts

--- OtherFiles/ubuntuinstall.sh
+++ OtherFiles/ubuntuinstall.sh
@@ -6,12 +6,32 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 
+#install git
+sudo apt install git -y
+
+#install snap and vscode
+sudo apt install snapd -y
+sudo apt install code --classic
+
+#install curl
+sudo apt install curl -y
+
 # Install Apache, MySQL, PHP 8.2 and required extensions
 sudo apt-get install -y apache2 mysql-server php8.2 libapache2-mod-php8.2 php8.2-mysql php8.2-mbstring php8.2-xml php8.2-bcmath php8.2-json php8.2-curl unzip
 
 # Enable PHP 8.2 and configure Apache to use it
 sudo a2enmod php8.2
-sudo systemctl restart apache2
 
 # Install additional extensions
 sudo apt-get install -y php8.2-mbstring php8.2-xml php8.2-bcmath php8.2-json php8.2-curl php8.2-zip
+
+sudo systemctl start apache2
+
+sudo systemctl enable apache2
+
+sudo systemctl start mysql
+
+sudo systemctl enable mysql
+
+
+echo "Installation complete"

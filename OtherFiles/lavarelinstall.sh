35787a15e73e3cc2700c790e572691abbe346c77
vboxuser <vboxuser@proj480-del.myguest.virtualbox.org>
2024-11-15 01:04:29 -0800

    a

--- OtherFiles/lavarelinstall.sh
+++ OtherFiles/lavarelinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #you might have to manually install this part for laravel
-cd /var/www/
-git clone https://github.com/Alexisb710/project480.git
-cd project480
+cd /var/www/project480
+#git clone https://github.com/Alexisb710/project480.git
+#cd project480
 
 # Install Composer dependencies
 composer install
@@ -11,12 +11,10 @@ composer install
 cp .env.example .env
 
 # Replace DB_DATABASE, DB_USERNAME, and DB_PASSWORD values in .env
-# sudo nano .env
-echo "Updating database configuration in .env file..."
 sed -i 's/^DB_DATABASE=.*/DB_DATABASE=project480/' .env
 sed -i 's/^DB_USERNAME=.*/DB_USERNAME=admin/' .env
 sed -i 's/^DB_PASSWORD=.*/DB_PASSWORD=password/' .env
-
+#
 # Generate application key
 php artisan key:generate
 
